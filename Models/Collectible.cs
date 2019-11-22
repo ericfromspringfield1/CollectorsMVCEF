a1c88957272bfd143bf28aaedae017eb245ab00e
ericfromspringfield1 <ericfromsrpingfield@gmail.com>
2019-11-22 15:27:32 -0600

    edit, delete, default date

---
+++ Models/Collectible.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Collectors.Models
+{
+    public class Collectible
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(55)]
+        public string Name { get; set; }
+        [MaxLength(4000)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CollectedDate { get; set; }
+        public Collectible()
+        {
+            CollectedDate = DateTime.Now;
+
+        }
+
+
+        public int CollectionId { get; set; }
+        public Collection Collection { get; set; } 
+    }
+}
---
+++ Models/CollectibleCreateViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Collectors.Models
+{
+    public class CollectibleCreateViewModel
+    {
+        public Collectible Collectible { get; set; }
+
+        
+        public List<Collection> Collections { get; set; }
+
+        public List<SelectListItem> CollectionOptions
+        {
+            get
+            {
+                return Collections?.Select(c => new SelectListItem(c.Name, c.Id.ToString())).ToList();
+            }
+        }
+    }
+}
